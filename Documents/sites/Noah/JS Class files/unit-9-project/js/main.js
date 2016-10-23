4892d9c7cade39e6e0d933db7c7926c49d5cff1d
Noah <noah.klein@gmail.com>
2016-10-23 12:44:21 -0400

    first commit

---
+++ Documents/sites/Noah/JS Class files/unit-9-project/js/main.js
@@ -0,0 +1,10 @@
+function initMap(){
+	var map = new google.maps.Map(document.getElementById('map'),{
+		center: {lat: 40.8054491, lng: -73.9654415},
+		zoom: 20
+	});
+	   var marker = new google.maps.Marker({
+          position: {lat: 40.8054491, lng: -73.9654415},
+          map: map
+        });
+}
