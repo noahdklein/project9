fd04494f48219bb7b7157f7575ce05917fcdc047
Noah <noah.klein@gmail.com>
2016-11-01 13:30:35 -0400

    js update

--- Documents/sites/Noah/JS Class files/unit-9-project/js/main.js
+++ Documents/sites/Noah/JS Class files/unit-9-project/js/main.js
@@ -8,3 +8,48 @@ function initMap(){
           map: map
         });
 }
+var nameInputField = document.getElementById('submit-res');
+
+nameInputField.addEventListener('click', checkNameInput);
+
+function checkNameInput(){
+	if (nameInputField.value.length === 0){
+		document.getElementById('message').innerText = "please enter your information."
+		nameInputField.className = 'error';
+	} else {
+		document.getElementById('message').innerText = '';
+		nameInputField.className = '';
+	}
+};
+var dayInputField = document.getElementById('submit-res');
+
+dayInputField.addEventListener('click', checkDayInput);
+
+function checkDayInput(){
+	if (dayInputField.value.length === 0){
+		document.getElementById('message').innerText = "please enter your information."
+		dayInputField.className = 'error';
+	} else {
+		document.getElementById('message').innerText = '';
+		dayInputField.className = '';
+	}
+};
+
+function addName(){
+	var inputName = document.getElementById('Name');
+	var newNameText = inputName.value; inputName.value = '';
+
+	var resName = document.createElement('td');
+	resName.innerText = newNameText;
+	document.getElementById('res-name').appendChild(resName);
+};
+document.getElementsByTagName('button')[0].addEventListener('click', addName);
+function addDay(){
+	var inputDay = document.getElementById('Day');
+	var newDayText = inputDay.value; inputDay.value = '';
+
+	var resDay = document.createElement('td');
+	resDay.innerText = newDayText;
+	document.getElementById('res-day').appendChild(resDay);
+};
+document.getElementsByTagName('button')[0].addEventListener('click', addDay);
