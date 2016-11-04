c15f8fca0a30113aa54461ff1717aff42374c21f
Noah <noah.klein@gmail.com>
2016-11-04 14:30:38 -0400

    updateJS

--- Documents/sites/Noah/JS Class files/unit-9-project/js/main.js
+++ Documents/sites/Noah/JS Class files/unit-9-project/js/main.js
@@ -8,53 +8,52 @@ function initMap(){
           map: map
         });
 }
+// end map
 var nameInputField = document.getElementById('submit-res');
-
-nameInputField.addEventListener('click', checkNameInput);
+var checkNameField = document.getElementById('Name');
+var checkDayField = document.getElementById('Day');
 
 function checkNameInput(e){
-	e.preventDefault();
-	if (nameInputField.value.length === 0){
+	if (checkNameField.value.length === 0){
 		document.getElementById('message').innerText = "please enter your information."
-		nameInputField.className = 'error';
+		checkNameField.className = 'error';
 		return false;
 	} else {
 		document.getElementById('message').innerText = '';
-		nameInputField.className = '';
+		checkNameField.className = '';
+		return true;
 	}
 };
-var dayInputField = document.getElementById('submit-res');
-
-dayInputField.addEventListener('click', checkDayInput);
-
 function checkDayInput(e){
-	e.preventDefault();
-	if (dayInputField.value.length === 0){
+	
+	if (checkDayField.value.length === 0){
 		document.getElementById('message').innerText = "please enter your information."
-		dayInputField.className = 'error';
+		checkDayField.className = 'error';
 		return false;
 	} else {
 		document.getElementById('message').innerText = '';
-		dayInputField.className = '';
+		checkDayField.className = '';
+		return true;
 	}
 };
-
 function addRes(e){
-	e.preventDefault();
-
 	var table = document.getElementById('resTable');
 	var newRow = table.insertRow(1);
 	var cell1 = newRow.insertCell(0);
 	var cell2 = newRow.insertCell(1);
 	var inputName = document.getElementById('Name');
 	var newNameText = inputName.value; inputName.value = '';
-
 	var inputDay = document.getElementById('Day');
 	var newDayText = inputDay.value; inputDay.value = '';
-
 	cell1.innerHTML = newNameText;
-	cell2.innerHTML = newDayText;
-
-	
+	cell2.innerHTML = newDayText;	
 };
-document.getElementsByTagName('button')[0].addEventListener('click', addRes);
+function checkForm(e){
+e.preventDefault();
+checkNameInput();
+checkDayInput();
+if (checkNameInput() && checkNameInput() === true){
+	addRes();
+}
+};
+document.getElementsByTagName('button')[0].addEventListener('click', checkForm);
